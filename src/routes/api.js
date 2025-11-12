79e58a3af54152f135950e874b4e16cce361e95f
Nguyen Quoc Do <nguyenquocdodary@gmail.com>
2025-11-12 11:08:20 +0700

    create new user complete

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,7 @@
 import express from "express";
 import apiController from "../controllers/apiController";
 import userController from "../controllers/userController";
+import groupController from "../controllers/groupController";
 const router = express.Router();
 
 const initApiRoutes = (app) => {
@@ -13,6 +14,8 @@ const initApiRoutes = (app) => {
     router.post("/user/create", userController.createFunc);
     router.put("/user/update", userController.updateFunc);
     router.delete("/user/delete", userController.deleteFunc);
+
+    router.get("/group/read", groupController.readFunc)
 
     return app.use("/api/v1", router);
 
